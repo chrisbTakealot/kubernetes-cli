bdb0432e6db424c920b074551464bf3d0c81b4ad
chrisbTakealot <christopher@takealot.com>
2017-05-18 11:59:11 +0200

    wip

--- lib/talkube.py
+++ lib/talkube.py
@@ -4,18 +4,49 @@ import os
 class talkube():
 	v1 = None
 
+	
 	def __init__(self):
 		# load config
 		config.load_kube_config()	
 		self.v1 = client.CoreV1Api()
 
+
+	def print_header(self, header_name):
+		print header_name
+		print "-" * len(header_name)
+
+	
 	def list_pods(self):
 		"""
 		List all pods
 
 		"""
-		print "Listing pods with their IPs:"
-		print "----------------------------"
+		self.print_header("Listing pods with their IPs:")
 		ret = self.v1.list_pod_for_all_namespaces(watch=False)
 		for i in ret.items:
 		    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+	def pod_find(self, podname, namespace=None):
+		"""
+		Find a pod by namespace (Default all) and partial podname
+
+		"""
+		self.print_header("Looking for pod with name: %s in namespace: %s" % (podname, namespace if namespace else "All"))
+		ret = self.v1.list_pod_for_all_namespaces(watch=False)
+		for i in ret.items:
+			if(podname in i.metadata.name) and (namespace is None or namespace == i.metadata.namespace):
+				print "Matched %s" % i.metadata.name
+				return  i.metadata.name
+		print "* No match found *"
+		return None
+
+
+	def pod_bash(self, podname):
+		"""
+		Connect to pod 	    	
+		$ kubectl exec -it  $_PODNAME_$ -- /bin/bash
+    	"""
+		
+		self.print_header("Connecting to bash for pod: %s" % podname)
+		os.system("kubectl exec -it %s -- /bin/bash" % podname)
--- talkube.py
+++ talkube.py
@@ -1,20 +1,105 @@
-import lib.talkube
+from kubernetes import client, config
 import os
+from optparse import OptionParser
 
-def main():
-	print """
- _______       _      _  __     _          
-|__   __|/\   | |    | |/ /    | |         
-   | |  /  \  | |    | ' /_   _| |__   ___ 
-   | | / /\ \ | |    |  <| | | | '_ \ / _ \\
-   | |/ ____ \| |____| . \ |_| | |_) |  __/
-   |_/_/    \_\______|_|\_\__,_|_.__/ \___|
-         Takealot Kubernetes CLI Tools
-         """
 
-	tkube = lib.talkube.talkube()
-	tkube.list_pods()
+class talkube():
+	v1 = None
+
+	
+	def __init__(self):
+		# load config
+		config.load_kube_config()	
+		self.v1 = client.CoreV1Api()
+
+
+	def print_header(self, header_name):
+		print header_name
+		print "-" * len(header_name)
+
+	
+	def list_pods(self):
+		"""
+		List all pods
+
+		"""
+		self.print_header("Listing pods with their IPs:")
+		ret = self.v1.list_pod_for_all_namespaces(watch=False)
+		for i in ret.items:
+		    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+		print
+
+	def pod_find(self, podname, namespace=None):
+		"""
+		Find a pod by namespace (Default all) and partial podname
+
+		"""
+		self.print_header("Looking for pod with name: %s in namespace: %s" % (podname, namespace if namespace else "All"))
+		ret = self.v1.list_pod_for_all_namespaces(watch=False)
+		for i in ret.items:
+			if(podname in i.metadata.name) and (namespace is None or namespace == i.metadata.namespace):
+				print "Matched %s" % i.metadata.name
+				return  i.metadata.name
+		print "* No match found *"
+		return None
+
+
+	def pod_bash(self, podname):
+		"""
+		Connect to pod 	    	
+		$ kubectl exec -it  $_PODNAME_$ -- /bin/bash
+    	"""
+		
+		self.print_header("Connecting to bash for pod: %s" % podname)
+		os.system("kubectl exec -it %s -- /bin/bash" % podname)
+
 
 
 if __name__ == "__main__":
-	main()
+	try:
+		print """
+	 _______       _      _  __     _          
+	|__   __|/\   | |    | |/ /    | |         
+	   | |  /  \  | |    | ' /_   _| |__   ___ 
+	   | | / /\ \ | |    |  <| | | | '_ \ / _ \\
+	   | |/ ____ \| |____| . \ |_| | |_) |  __/
+	   |_/_/    \_\______|_|\_\__,_|_.__/ \___|
+	         Takealot Kubernetes CLI Tools
+	         """
+
+		tkube = talkube()
+		tkube.list_pods()
+		
+		usage = "usage: %prog [options] podname"
+		parser = OptionParser(description="A CLI tool for kubernetes", usage=usage)
+
+		parser.add_option("-l", "--list", dest="pod_list",
+	                      help="List all pod names", 
+	                      type=str,
+	                      default=None)
+
+		parser.add_option("-n", "--namespace", dest="pod_namespace",
+	                      help="Namespace for pods [default 'All']", 
+	                      type=str,
+	                      default=None)
+
+
+		parser.add_option("-b", "--bash", dest="pod_bash",
+	                      help="Run bash on pod (using podname match)", 
+	                      type=int,
+	                      default=False)
+
+
+		(options, args) = parser.parse_args()
+		if len(args) != 1:
+		    print parser.print_help()
+		    exit()
+
+		podname = tkube.pod_find(podname=args[0])
+		if podname is None:
+			raise Exception("Podname %s not found" % podname)
+		# tkube.pod_find(podname="cs-admin-bff")
+		# tkube.pod_bash()
+	except Exception as e:
+		print
+		print "!!! Error: %s !!!" % e.message
